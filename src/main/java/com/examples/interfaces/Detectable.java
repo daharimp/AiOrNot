7b7d12d3deb3abe836c237e18d9c03779fcd1678
daharimp <daharimorgan@gmail.com>
2026-05-19 09:41:57 -0700

    Initial Instance commit, implemented Detectable instance as well as multimedia abstract class.

--- src/main/java/com/examples/interfaces/Detectable.java
+++ src/main/java/com/examples/interfaces/Detectable.java
@@ -1,5 +1,7 @@
 package com.examples.interfaces;
 
+// This interface defines methods for analyzing content and determining if it is AI-related.
+
 public interface Detectable {
     void analyseContent();
     boolean isAI();
